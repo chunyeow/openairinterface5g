f0cf6ab6145d92e1a25c60ac24d27cb6628cef96
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2013-07-05 10:58:48 +0000

    Last commit was a bad commit for this file
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@4008 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair1/SIMULATION/ETH_TRANSPORT/netlink_init.c
+++ openair1/SIMULATION/ETH_TRANSPORT/netlink_init.c
@@ -38,65 +38,53 @@ int netlink_init(void)
 
     nas_sock_fd = socket(PF_NETLINK, SOCK_RAW,GRAAL_NETLINK_ID);
     if (nas_sock_fd == -1) {
-        printf("[NETLINK] Error opening socket %d\n",nas_sock_fd);
-        return(-1);
+        printf("[NETLINK] Error opening socket %d (%d:%s)\n",nas_sock_fd,errno, strerror(errno));
+        exit(1);
     }
     printf("[NETLINK]Opened socket with fd %d\n",nas_sock_fd);
 
     ret = fcntl(nas_sock_fd,F_SETFL,O_NONBLOCK);
-    printf("[NETLINK] fcntl returns %d\n",ret);
-
-  
-  nas_sock_fd = socket(PF_NETLINK, SOCK_RAW,GRAAL_NETLINK_ID);
-  if (nas_sock_fd==-1) {
-      printf("[NETLINK] Error opening socket %d (%d:%s)\n",nas_sock_fd,errno, strerror(errno));
-      exit(1);
-  }  
-  printf("[NETLINK]Opened socket with fd %d\n",nas_sock_fd);
-
-
-    printf("[NETLINK] bind returns %d\n",ret);
-  if (ret == -1) {
+    if (ret == -1) {
       printf("[NETLINK] Error fcntl (%d:%s)\n",errno, strerror(errno));
       exit(1);
-  }
+    }
 
-  memset(&nas_src_addr, 0, sizeof(nas_src_addr));
-  nas_src_addr.nl_family = AF_NETLINK;
-  nas_src_addr.nl_pid = 1;//getpid();  /* self pid */
-  nas_src_addr.nl_groups = 0;  /* not in mcast groups */
-  ret = bind(nas_sock_fd, (struct sockaddr*)&nas_src_addr, sizeof(nas_src_addr));
+    memset(&nas_src_addr, 0, sizeof(nas_src_addr));
+    nas_src_addr.nl_family = AF_NETLINK;
+    nas_src_addr.nl_pid = 1;//getpid();  /* self pid */
+    nas_src_addr.nl_groups = 0;  /* not in mcast groups */
+    ret = bind(nas_sock_fd, (struct sockaddr*)&nas_src_addr, sizeof(nas_src_addr));
 
 
 
-  memset(&nas_dest_addr, 0, sizeof(nas_dest_addr));
-  nas_dest_addr.nl_family = AF_NETLINK;
-  nas_dest_addr.nl_pid = 0;   /* For Linux Kernel */
-  nas_dest_addr.nl_groups = 0; /* unicast */
+    memset(&nas_dest_addr, 0, sizeof(nas_dest_addr));
+    nas_dest_addr.nl_family = AF_NETLINK;
+    nas_dest_addr.nl_pid = 0;   /* For Linux Kernel */
+    nas_dest_addr.nl_groups = 0; /* unicast */
   
-  // TX PART
-  nas_nlh_tx=(struct nlmsghdr *)malloc(NLMSG_SPACE(NL_MAX_PAYLOAD));
-  memset(nas_nlh_tx, 0, NLMSG_SPACE(NL_MAX_PAYLOAD));
-  /* Fill the netlink message header */
-  nas_nlh_tx->nlmsg_len = NLMSG_SPACE(NL_MAX_PAYLOAD);
-  nas_nlh_tx->nlmsg_pid = 1;//getpid();  /* self pid */
-  nas_nlh_tx->nlmsg_flags = 0;
+    // TX PART
+    nas_nlh_tx=(struct nlmsghdr *)malloc(NLMSG_SPACE(NL_MAX_PAYLOAD));
+    memset(nas_nlh_tx, 0, NLMSG_SPACE(NL_MAX_PAYLOAD));
+    /* Fill the netlink message header */
+    nas_nlh_tx->nlmsg_len = NLMSG_SPACE(NL_MAX_PAYLOAD);
+    nas_nlh_tx->nlmsg_pid = 1;//getpid();  /* self pid */
+    nas_nlh_tx->nlmsg_flags = 0;
   
-  nas_iov_tx.iov_base = (void *)nas_nlh_tx;
-  nas_iov_tx.iov_len = nas_nlh_tx->nlmsg_len;
-  memset(&nas_msg_tx,0,sizeof(nas_msg_tx));
-  nas_msg_tx.msg_name = (void *)&nas_dest_addr;
-  nas_msg_tx.msg_namelen = sizeof(nas_dest_addr);
-  nas_msg_tx.msg_iov = &nas_iov_tx;
-  nas_msg_tx.msg_iovlen = 1;
+    nas_iov_tx.iov_base = (void *)nas_nlh_tx;
+    nas_iov_tx.iov_len = nas_nlh_tx->nlmsg_len;
+    memset(&nas_msg_tx,0,sizeof(nas_msg_tx));
+    nas_msg_tx.msg_name = (void *)&nas_dest_addr;
+    nas_msg_tx.msg_namelen = sizeof(nas_dest_addr);
+    nas_msg_tx.msg_iov = &nas_iov_tx;
+    nas_msg_tx.msg_iovlen = 1;
   
   
-  // RX PART
-  memset(&nas_msg_rx,0,sizeof(nas_msg_rx));
-  nas_msg_rx.msg_name = (void *)&nas_src_addr;
-  nas_msg_rx.msg_namelen = sizeof(nas_src_addr);
-  nas_msg_rx.msg_iov = &nas_iov_rx;
-  nas_msg_rx.msg_iovlen = 1;
+    // RX PART
+    memset(&nas_msg_rx,0,sizeof(nas_msg_rx));
+    nas_msg_rx.msg_name = (void *)&nas_src_addr;
+    nas_msg_rx.msg_namelen = sizeof(nas_src_addr);
+    nas_msg_rx.msg_iov = &nas_iov_rx;
+    nas_msg_rx.msg_iovlen = 1;
 
     return(nas_sock_fd);
 }
