239fd2febf0919887cb461b32af97f774b84b58e
Florian Kaltenberger <florian.kaltenberger@eurecom.fr>
2013-09-20 13:39:27 +0000

    adding pdcch_eval_results.m
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@4160 818b1a75-f10b-46b9-bf7c-635c3b92a50f

---
+++ openair1/SIMULATION/LTE_PHY/pdcch_eval_results.m
@@ -0,0 +1,33 @@
+set(0, 'DefaultLineMarkerSize', 10);
+set(0, 'Defaultaxesfontsize', 14);
+set(0, 'DefaultLineLineWidth', 2);
+plot_style = {'b-o';'r-x';'g-d';'y-*';'k-s'};
+
+SNR = -5:0.2:2.8;
+C = zeros(length(SNR),4);
+filebase = 'pdcch_fdd_10_siso_awgn_ic_format0';
+
+figure(1)
+hold off
+for L=0:3
+    fid = fopen(sprintf('%s_L%d.txt',filebase,L),'r');
+    [A,c]=fscanf(fid,'SNR %f : n_errors_ul = %d/%d (%f)\n');
+    fclose(fid);
+
+    B = reshape(A,4,40).';
+    C(:,L+1) = B(:,4); 
+    
+    semilogy(B(:,1),B(:,4),plot_style{L+1})
+    hold on
+    
+    legend_str{L+1} = sprintf('L=%d',L);
+end
+
+%%
+h = legend(legend_str);
+set(h,'Fontsize',14);
+title(filebase,'Fontsize',16,'Interpreter','none')
+xlabel('SNR','Fontsize',14)
+ylabel('BLER','Fontsize',14)
+grid on
+saveas(gcf, filebase, 'eps');
