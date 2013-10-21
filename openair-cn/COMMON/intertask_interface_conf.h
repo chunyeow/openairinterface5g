fe8a887b1aeb2118ccd8323500d4453e3de64a77
winckel <winckel@eurecom.fr>
2013-10-21 17:09:39 +0000

    Created "intertask_interface_conf.h" to defines some parameters that were defined in "mme_default_values.h" to make ITTI more generic.
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@4243 818b1a75-f10b-46b9-bf7c-635c3b92a50f

---
+++ openair-cn/COMMON/intertask_interface_conf.h
@@ -0,0 +1,51 @@
+/*******************************************************************************
+
+  Eurecom OpenAirInterface
+  Copyright(c) 1999 - 2012 Eurecom
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms and conditions of the GNU General Public License,
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+
+  The full GNU General Public License is included in this distribution in
+  the file called "COPYING".
+
+  Contact Information
+  Openair Admin: openair_admin@eurecom.fr
+  Openair Tech : openair_tech@eurecom.fr
+  Forums       : http://forums.eurecom.fr/openairinterface
+  Address      : EURECOM, Campus SophiaTech, 450 Route des Chappes
+                 06410 Biot FRANCE
+
+*******************************************************************************/
+
+/*
+ * intertask_interface_conf.h
+ *
+ *  Created on: Oct 21, 2013
+ *      Author: winckel
+ */
+
+#ifndef INTERTASK_INTERFACE_CONF_H_
+#define INTERTASK_INTERFACE_CONF_H_
+
+/*******************************************************************************
+ * Intertask Interface Constants
+ ******************************************************************************/
+
+#define ITTI_QUEUE_SIZE_PER_TASK (5 * 1024 * 1024) /* Limit the queue size to 5M */
+#define ITTI_PORT                (10007)
+/* This is the queue size for signal dumper */
+#define ITTI_QUEUE_SIZE_MAX      (1 * 1024 * 1024) /* 1 MBytes */
+#define ITTI_DUMP_MAX_CON        (5)    /* Max connections in parallel */
+
+#endif /* INTERTASK_INTERFACE_CONF_H_ */
