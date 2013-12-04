ff4b9a11ec784c3e4503cfcbff05ab06fbcc57ed
winckel <winckel@eurecom.fr>
2013-12-04 08:25:06 +0000

    Added a define to access TIMER_HAS_EXPIRED fields.
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@4571 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- common/utils/itti/timer_messages_types.h
+++ common/utils/itti/timer_messages_types.h
@@ -1,6 +1,11 @@
 #ifndef TIMER_MESSAGES_TYPES_H_
 #define TIMER_MESSAGES_TYPES_H_
 
+//-------------------------------------------------------------------------------------------//
+// Defines to access message fields.
+#define TIMER_HAS_EXPIRED(mSGpTR)   (mSGpTR)->ittiMsg.timer_has_expired
+
+//-------------------------------------------------------------------------------------------//
 typedef struct {
     void *arg;
     long  timer_id;
