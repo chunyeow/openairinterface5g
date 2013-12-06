70ed03a89ac8f4e286c5a73fa7032c78819cfb35
winckel <winckel@eurecom.fr>
2013-12-06 14:10:27 +0000

    Reordered ITTI tasks.
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@4629 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair2/COMMON/tasks_def.h
+++ openair2/COMMON/tasks_def.h
@@ -1,42 +1,42 @@
 // This task is mandatory and must always be placed in first position
-TASK_DEF(TASK_TIMER, TASK_PRIORITY_MAX, 10)
+    TASK_DEF(TASK_TIMER,    TASK_PRIORITY_MAX,          10)
 
 // Other possible tasks in the process
 
-/// Layer 2 and Layer 1 task supporting all the synchronous processing
-TASK_DEF(TASK_L2L1, TASK_PRIORITY_MAX_LEAST, 1000)
-//// Layer 2 and Layer 1 sub-tasks
-SUB_TASK_DEF(TASK_L2L1, TASK_PDCP_UE, 200)
-SUB_TASK_DEF(TASK_L2L1, TASK_PDCP_ENB, 200)
+// Common tasks:
+    /// Layer 2 and Layer 1 task supporting all the synchronous processing
+    TASK_DEF(TASK_L2L1,     TASK_PRIORITY_MAX_LEAST,    200)
 
-SUB_TASK_DEF(TASK_L2L1, TASK_RLC_UE, 200)
-SUB_TASK_DEF(TASK_L2L1, TASK_RLC_ENB, 200)
+    ///   Bearers Manager task
+    TASK_DEF(TASK_BM,       TASK_PRIORITY_MED,          200)
 
-SUB_TASK_DEF(TASK_L2L1, TASK_MAC_UE, 200)
-SUB_TASK_DEF(TASK_L2L1, TASK_MAC_ENB, 200)
+// eNodeB tasks and sub-tasks:
+    //// Layer 2 and Layer 1 sub-tasks
+SUB_TASK_DEF(TASK_L2L1,     TASK_PHY_ENB,               200)
+SUB_TASK_DEF(TASK_L2L1,     TASK_MAC_ENB,               200)
+SUB_TASK_DEF(TASK_L2L1,     TASK_RLC_ENB,               200)
+SUB_TASK_DEF(TASK_L2L1,     TASK_PDCP_ENB,              200)
 
-SUB_TASK_DEF(TASK_L2L1, TASK_PHY_UE, 200)
-SUB_TASK_DEF(TASK_L2L1, TASK_PHY_ENB, 200)
+    ///   Radio Resource Control task
+    TASK_DEF(TASK_RRC_ENB,  TASK_PRIORITY_MED,          200)
+    ///   S1ap task
+    TASK_DEF(TASK_S1AP,     TASK_PRIORITY_MED,          200)
+    ///   X2ap task, acts as both source and target
+    TASK_DEF(TASK_X2AP,     TASK_PRIORITY_MED,          200)
+    ///   Sctp task (Used by both S1AP and X2AP)
+    TASK_DEF(TASK_SCTP,     TASK_PRIORITY_MED,          200)
+    ///   eNB APP task
+    TASK_DEF(TASK_ENB_APP,  TASK_PRIORITY_MED,          200)
 
-/// Radio Resource Control task for UE
-TASK_DEF(TASK_RRC_UE, TASK_PRIORITY_MED, 200)
-/// Radio Resource Control task for eNodeB
-TASK_DEF(TASK_RRC_ENB, TASK_PRIORITY_MED, 200)
+// UE tasks and sub-tasks:
+    //// Layer 2 and Layer 1 sub-tasks
+SUB_TASK_DEF(TASK_L2L1,     TASK_PHY_UE,                200)
+SUB_TASK_DEF(TASK_L2L1,     TASK_MAC_UE,                200)
+SUB_TASK_DEF(TASK_L2L1,     TASK_RLC_UE,                200)
+SUB_TASK_DEF(TASK_L2L1,     TASK_PDCP_UE,               200)
 
-/// Bearers Manager task
-TASK_DEF(TASK_BM, TASK_PRIORITY_MED, 200)
+    ///   Radio Resource Control task
+    TASK_DEF(TASK_RRC_UE,   TASK_PRIORITY_MED,          200)
+    ///   Non Access Stratum task
+    TASK_DEF(TASK_NAS_UE,   TASK_PRIORITY_MED,          200)
 
-/// Non Access Stratum task for UE
-TASK_DEF(TASK_NAS_UE, TASK_PRIORITY_MED, 200)
-
-/// S1ap task for eNodeB
-TASK_DEF(TASK_S1AP, TASK_PRIORITY_MED, 200)
-
-/// Sctp task for eNodeB (Used by both S1AP and X2AP)
-TASK_DEF(TASK_SCTP, TASK_PRIORITY_MED, 200)
-
-/// X2ap task for eNodeB, acts as both source and target
-TASK_DEF(TASK_X2AP, TASK_PRIORITY_MED, 200)
-
-/// eNB APP task
-TASK_DEF(TASK_ENB_APP, TASK_PRIORITY_MED, 200)
