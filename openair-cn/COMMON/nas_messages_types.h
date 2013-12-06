e6b253a0b2e4248667394c9aa15cbbff1bc81b70
winckel <winckel@eurecom.fr>
2013-12-06 15:25:57 +0000

    Added struc names.
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@4631 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair-cn/COMMON/nas_messages_types.h
+++ openair-cn/COMMON/nas_messages_types.h
@@ -14,11 +14,11 @@
 #define NAS_AUTHENTICATION_PARAM_RSP(mSGpTR)    (mSGpTR)->ittiMsg.nas_auth_param_rsp
 #define NAS_AUTHENTICATION_PARAM_FAIL(mSGpTR)   (mSGpTR)->ittiMsg.nas_auth_param_fail
 
-typedef struct {
+typedef struct nas_paging_ind_s {
     
 } nas_paging_ind_t;
 
-typedef struct {
+typedef struct nas_conn_est_ind_s {
     nas_establish_ind_t nas;
 
     /* Transparent message from s1ap to be forwarded to MME_APP or
@@ -62,7 +62,7 @@ typedef struct nas_bearer_param_s {
     ip_address_t s_gw_address;
 } nas_bearer_param_t;
 
-typedef struct {
+typedef struct nas_conn_rel_ind_s {
     
 } nas_conn_rel_ind_t;
 
@@ -71,23 +71,23 @@ typedef ul_info_transfer_ind_t nas_ul_data_ind_t;
 typedef dl_info_transfer_req_t nas_dl_data_req_t;
 typedef dl_info_transfer_cnf_t nas_dl_data_cnf_t;
 
-typedef struct {
+typedef struct nas_non_del_ind_s {
     
 } nas_non_del_ind_t;
 
-typedef struct {
+typedef struct nas_rab_est_req_s {
     
 } nas_rab_est_req_t;
 
-typedef struct {
+typedef struct nas_rab_est_rsp_s {
     
 } nas_rab_est_rsp_t;
 
-typedef struct {
+typedef struct nas_rab_rel_req_s {
     
 } nas_rab_rel_req_t;
 
-typedef struct {
+typedef struct nas_attach_req_s {
     /* TODO: Set the correct size */
     char apn[100];
     char imsi[16];
@@ -96,7 +96,7 @@ typedef struct {
     s1ap_initial_ue_message_t transparent;
 } nas_attach_req_t;
 
-typedef struct {
+typedef struct nas_auth_req_s {
     /* UE imsi */
     char imsi[16];
 
@@ -106,7 +106,7 @@ typedef struct {
     int cause;
 } nas_auth_req_t;
 
-typedef struct {
+typedef struct nas_auth_resp_s {
     char imsi[16];
 } nas_auth_resp_t;
 
