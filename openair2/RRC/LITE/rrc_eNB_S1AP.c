f8d5dc92254d5bb43b62db8eabb97c457b0dac2d
Cedric Roux <cedric.roux@eurecom.fr>
2013-12-17 08:52:32 +0000

    - Fix eNB_ue_s1ap_id not set correctly when receiving First income from MME
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@4749 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair2/RRC/LITE/rrc_eNB_S1AP.c
+++ openair2/RRC/LITE/rrc_eNB_S1AP.c
@@ -452,6 +452,10 @@ int rrc_eNB_process_S1AP_DOWNLINK_NAS(MessageDef *msg_p, const char *msg_name, i
     return (-1);
   }
   else {
+    /* Is it the first income from S1AP ? */
+    if (eNB_rrc_inst[instance].Info.UE[ue_index].eNB_ue_s1ap_id == 0) {
+      eNB_rrc_inst[instance].Info.UE[ue_index].eNB_ue_s1ap_id = S1AP_DOWNLINK_NAS (msg_p).eNB_ue_s1ap_id;
+    }
     /* Create message for PDCP (DLInformationTransfer_t) */
     length = do_DLInformationTransfer (&buffer, rrc_eNB_get_next_transaction_identifier (instance),
                                        S1AP_DOWNLINK_NAS (msg_p).nas_pdu.length,
