26e981d6a62a86bcf588b162c8b18425e97c5d27
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2014-03-03 16:17:49 +0000

    Explicit use 3GPP_ADDR
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5106 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- targets/PROJECTS/SPECTRA/build_all.bash
+++ targets/PROJECTS/SPECTRA/build_all.bash
@@ -37,8 +37,8 @@ echo_success "\n###############################"
 echo_success "# COMPILE oaisim"
 echo_success "###############################"
 cd $OPENAIR_TARGETS/SIMU/USER
-echo_success "Executing: make oaisim NAS=1 OAI_NW_DRIVER_TYPE_ETHERNET=1 ENABLE_ITTI=1 USER_MODE=1 OPENAIR2=1 ENABLE_RAL=1 MIH_C_MEDIEVAL_EXTENSIONS=1 RLC_STOP_ON_LOST_PDU=1 Rel10=1 -j`grep -c ^processor /proc/cpuinfo `"
-make --keep-going oaisim NAS=1 OAI_NW_DRIVER_TYPE_ETHERNET=1 ENABLE_ITTI=1 USER_MODE=1 OPENAIR2=1 ENABLE_RAL=1 MIH_C_MEDIEVAL_EXTENSIONS=1 RLC_STOP_ON_LOST_PDU=1 Rel10=1 -j`grep -c ^processor /proc/cpuinfo `
+echo_success "Executing: make oaisim NAS=1 OAI_NW_DRIVER_TYPE_ETHERNET=1 ENABLE_ITTI=1 USER_MODE=1 OPENAIR2=1 ENABLE_RAL=1 MIH_C_MEDIEVAL_EXTENSIONS=1  USE_3GPP_ADDR_AS_LINK_ADDR=1 RLC_STOP_ON_LOST_PDU=1 Rel10=1 -j`grep -c ^processor /proc/cpuinfo `"
+make --keep-going oaisim NAS=1 OAI_NW_DRIVER_TYPE_ETHERNET=1 ENABLE_ITTI=1 USER_MODE=1 OPENAIR2=1 ENABLE_RAL=1 MIH_C_MEDIEVAL_EXTENSIONS=1 USE_3GPP_ADDR_AS_LINK_ADDR=1 RLC_STOP_ON_LOST_PDU=1 Rel10=1 -j`grep -c ^processor /proc/cpuinfo `
 if [[ $? -eq 2 ]] ; then
     exit 1
 fi
