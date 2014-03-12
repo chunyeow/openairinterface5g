70581d991e7cf5e7317b9efb960989dc52485b43
Florian Kaltenberger <florian.kaltenberger@eurecom.fr>
2014-03-12 10:48:20 +0000

    removing old (and wrong) AWGN reference curves
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5148 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair1/SIMULATION/LTE_PHY/BLER_SIMULATIONS/eval_results.m
+++ openair1/SIMULATION/LTE_PHY/BLER_SIMULATIONS/eval_results.m
@@ -2,15 +2,21 @@ set(0, 'DefaultLineMarkerSize', 10);
 set(0, 'Defaultaxesfontsize', 14);
 set(0, 'DefaultLineLineWidth', 2);
 
-root_path = 'Rayleigh8_perfectCE_new';
-tx_mode = 6;
-channel = 8;
+root_path = 'AWGN/AWGN_results';
+tx_mode = 1;
+nrx = 1;
+channel = 18;
+nb_rb = 25; %RB used
+nb_sf = 4;  %subframes per frame
 
 %%
 addpath('../../../EMOS/LTE/POST_PROCESSING')
 load SISO.mat
 
-scale_bps = (8*12+3*8)*6*100;
+%extended CP, 1PDCCH symbol
+%scale_bps = 25*(8*12+3*10)*6*100; 
+%normal CP, 1PDCCH symbol
+scale_bps = nb_rb*(10*12+3*10)*nb_sf*100;
 c_siso = [c_siso_4Qam; c_siso_16Qam; c_siso_64Qam];
 bps_siso = c_siso.*scale_bps;
 
@@ -33,7 +39,8 @@ hold off
 legend_str = {};
 i=1;
 for mcs=0:28
-    file = fullfile(root_path,sprintf('second_bler_tx%d_mcs%d_chan%d.csv',tx_mode,mcs,channel));
+    file = fullfile(root_path,sprintf('bler_tx%d_chan%d_nrx%d_mcs%d.csv',tx_mode,channel,nrx,mcs));
+    %file = fullfile(root_path,sprintf('awgn_bler_tx%d_mcs%d.csv',tx_mode,mcs));
     try
         data = dlmread(file,';',1,0);
     catch exception
@@ -51,13 +58,14 @@ for mcs=0:28
         uncoded_ber = ones(size(data,1),1);
     end
     if (length(snr)==1)
-    throughput_all(:,mcs+1) = (1-bler4).*harq_adjust.*get_tbs(mcs,25)*6*100;
+    throughput_all(:,mcs+1) = (1-bler4).*harq_adjust.*get_tbs(mcs,nb_rb)*nb_sf*100;
     else
-    throughput_all(:,mcs+1) = interp1(snr, (1-bler4).*harq_adjust.*get_tbs(mcs,25)*6*100,snr_all,'nearest','extrap');
+    throughput_all(:,mcs+1) = interp1(snr, (1-bler4).*harq_adjust.*get_tbs(mcs,nb_rb)*nb_sf*100,snr_all,'nearest','extrap');
     end
     throughput_all(1:find(snr_all==snr(1)),mcs+1) = 0;
     figure(1)
     semilogy(snr,bler(:,1),plot_style{mcs+1});
+    %snr10p(mcs+1) = interp1(bler(:,1),snr,0.1);
     hold on
     figure(2)
     semilogy(snr,uncoded_ber,plot_style{mcs+1});
@@ -70,10 +78,11 @@ end
 h_fig = figure(1);
 h_leg = legend(legend_str,'location','eastoutside');
 set(h_leg,'FontSize',10);
-title(sprintf('Tx mode %d: BLER vs SNR',tx_mode));
+title(sprintf('Tx mode %d, channel %d (%s): BLER vs SNR',tx_mode, channel,root_path));
 ylabel 'BLER'
 xlabel 'SNR'
 ylim([0.001 1])
+xlim([-10 40])
 grid on
 saveas(h_fig,fullfile(root_path,sprintf('tx_mode%d_channel%d_bler.fig',tx_mode,channel)));
 saveas(h_fig,fullfile(root_path,sprintf('tx_mode%d_channel%d_bler.emf',tx_mode,channel)));
