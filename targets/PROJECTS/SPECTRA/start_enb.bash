0af2657a546279c6a0ac30f68f7b00c80db05dce
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2014-03-14 09:44:09 +0000

    Licence
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5162 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- targets/PROJECTS/SPECTRA/start_enb.bash
+++ targets/PROJECTS/SPECTRA/start_enb.bash
@@ -1,6 +1,41 @@
 #!/bin/bash
-# Author Lionel GAUTHIER
-
+################################################################################
+# Eurecom OpenAirInterface core network
+# Copyright(c) 1999 - 2014 Eurecom
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# The full GNU General Public License is included in this distribution in
+# the file called "COPYING".
+#
+# Contact Information
+# Openair Admin: openair_admin@eurecom.fr
+# Openair Tech : openair_tech@eurecom.fr
+# Forums       : http://forums.eurecom.fsr/openairinterface
+# Address      : EURECOM,
+#                Campus SophiaTech,
+#                450 Route des Chappes,
+#                CS 50193
+#                06904 Biot Sophia Antipolis cedex,
+#                FRANCE
+################################################################################
+# file start_enb.bash
+# brief
+# author Lionel Gauthier
+# company Eurecom
+# email: lionel.gauthier@eurecom.fr
+#
 #------------------------------------------------
 # ENB CONFIG FILE
 #------------------------------------------------
