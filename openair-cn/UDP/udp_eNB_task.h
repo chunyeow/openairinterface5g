791e6a5f0f024f8391cd6c11035bdc3b435576c9
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2014-03-18 17:30:59 +0000

    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5186 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair-cn/UDP/udp_eNB_task.h
+++ openair-cn/UDP/udp_eNB_task.h
@@ -1,3 +1,40 @@
+/*******************************************************************************
+Eurecom OpenAirInterface core network
+Copyright(c) 1999 - 2014 Eurecom
+
+This program is free software; you can redistribute it and/or modify it
+under the terms and conditions of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
+
+This program is distributed in the hope it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+
+The full GNU General Public License is included in this distribution in
+the file called "COPYING".
+
+Contact Information
+Openair Admin: openair_admin@eurecom.fr
+Openair Tech : openair_tech@eurecom.fr
+Forums       : http://forums.eurecom.fsr/openairinterface
+Address      : EURECOM,
+               Campus SophiaTech,
+               450 Route des Chappes,
+               CS 50193
+               06904 Biot Sophia Antipolis cedex,
+               FRANCE
+*******************************************************************************/
+/*! \file udp_eNB_task.h
+* \brief
+* \author Sebastien ROUX, Lionel Gauthier
+* \company Eurecom
+* \email: lionel.gauthier@eurecom.fr
+*/
 #include <pthread.h>
 #include <stdint.h>
 
@@ -68,4 +105,4 @@ void *udp_eNB_task(void *args_p);
  */
 int udp_enb_init(const Enb_properties_t *enb_config_p);
 
-#endif /* UDP_PRIMITIVES_CLIENT_H_ */
+#endif /* UDP_ENB_TASK_H_ */
