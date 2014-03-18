df29718ae792d0b6f815a04784a0cddfa4ae68ec
Florian Kaltenberger <florian.kaltenberger@eurecom.fr>
2014-03-18 08:08:15 +0000

    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5178 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- targets/PROJECTS/CORRIDOR/emos_read.m
+++ targets/PROJECTS/CORRIDOR/emos_read.m
@@ -1,3 +1,6 @@
+close all
+clear all
+
 %% read data
 fid = fopen('E:\EMOS\corridor\eNB_data_20140108_185919.EMOS','r');
 [v,c]=fread(fid, 76800*4*100, 'int16',0,'ieee-le');
@@ -5,7 +8,7 @@ v2 = double(v(1:2:end))+1j*double(v(2:2:end));
 fclose(fid);
 
 figure(1)
-plot(abs(fft(v2)))
+plot(abs(fftshift(fft(v2))))
 
 %% sync
 addpath('../../../openair1/PHY/LTE_REFSIG');
@@ -17,27 +20,39 @@ figure(2);
 plot(lag,abs(corr));
 
 %% frame start detection
-% TODO: do a proper peak detection. for now just take the max
-[m,i]=max(abs(corr));
-frame_start = lag(i);
-
-%% ofdm receiver
-load('../LOCALIZATION/ofdm_pilots_sync_2048.mat');
+%load('../LOCALIZATION/ofdm_pilots_sync_2048.mat');
+load('E:\EMOS\corridor\ofdm_pilots_sync_2048_v7.mat');
 ofdm_symbol_length = num_carriers + prefix_length;
 frame_length = ofdm_symbol_length*num_symbols_frame;
 useful_carriers = num_carriers-num_zeros-1;
 
+% TODO: do a proper peak detection. for now just take the max
+[m,i]=max(abs(corr));
+frame_start = lag(i) - prefix_length;
+
+%% ofdm receiver
 received = v2(frame_start:frame_start+frame_length);
 received_f = zeros(num_symbols_frame,useful_carriers);
 for i=0:num_symbols_frame-1;
     ifblock=received(i*ofdm_symbol_length+[1:ofdm_symbol_length]);
     ifblock(1:prefix_length)=[];
     fblock=fft(ifblock);
-    received_f(i+1,:) = [fblock(end-useful_carriers/2+1:end); fblock(2:useful_carriers/2+1)];
+    received_f(i+1,:) = [fblock(2:useful_carriers/2+1); fblock(end-useful_carriers/2+1:end)];
 end
 
 %% channel estimation
 H=conj(f).*received_f;
-
+Ht = ifft(H,[],2);
+PDP = mean(abs(Ht(2:end,:).^2),1);
+figure(3)
+surf(20*log10(abs(Ht)))
+xlabel('time [OFDM symbol]')
+ylabel('delay time [samples]')
+zlabel('power [dB]')
+shading interp
+figure(4)
+plot(10*log10(PDP))
+xlabel('delay time [samples]')
+ylabel('power [dB]')
 
 
