81e8976c9ae0e11d73799c238f5b4c847703cb28
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2014-03-18 17:32:27 +0000

    format code
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5188 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair-cn/UDP/udp_primitives_server.c
+++ openair-cn/UDP/udp_primitives_server.c
@@ -1,32 +1,40 @@
 /*******************************************************************************
+Eurecom OpenAirInterface core network
+Copyright(c) 1999 - 2014 Eurecom
 
-  Eurecom OpenAirInterface
-  Copyright(c) 1999 - 2012 Eurecom
+This program is free software; you can redistribute it and/or modify it
+under the terms and conditions of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
 
-  This program is free software; you can redistribute it and/or modify it
-  under the terms and conditions of the GNU General Public License,
-  version 2, as published by the Free Software Foundation.
+This program is distributed in the hope it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
 
-  This program is distributed in the hope it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-  more details.
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
 
-  You should have received a copy of the GNU General Public License along with
-  this program; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+The full GNU General Public License is included in this distribution in
+the file called "COPYING".
 
-  The full GNU General Public License is included in this distribution in
-  the file called "COPYING".
-
-  Contact Information
-  Openair Admin: openair_admin@eurecom.fr
-  Openair Tech : openair_tech@eurecom.fr
-  Forums       : http://forums.eurecom.fr/openairinterface
-  Address      : EURECOM, Campus SophiaTech, 450 Route des Chappes
-                 06410 Biot FRANCE
-
+Contact Information
+Openair Admin: openair_admin@eurecom.fr
+Openair Tech : openair_tech@eurecom.fr
+Forums       : http://forums.eurecom.fsr/openairinterface
+Address      : EURECOM,
+               Campus SophiaTech,
+               450 Route des Chappes,
+               CS 50193
+               06904 Biot Sophia Antipolis cedex,
+               FRANCE
 *******************************************************************************/
+/*! \file udp_primitives_server.c
+* \brief
+* \author Sebastien ROUX, Lionel Gauthier
+* \company Eurecom
+* \email: lionel.gauthier@eurecom.fr
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -207,9 +215,12 @@ static void *udp_intertask_interface(void *args_p)
             case UDP_INIT: {
                 udp_init_t *udp_init_p;
                 udp_init_p = &received_message_p->ittiMsg.udp_init;
-                udp_create_socket(udp_init_p->port, udp_init_p->address,
-                                  ITTI_MSG_ORIGIN_ID(received_message_p));
+                udp_create_socket(
+                    udp_init_p->port,
+                    udp_init_p->address,
+                    ITTI_MSG_ORIGIN_ID(received_message_p));
             } break;
+
             case UDP_DATA_REQ: {
                 int     udp_sd = -1;
                 ssize_t bytes_written;
@@ -256,11 +267,14 @@ static void *udp_intertask_interface(void *args_p)
                     "(%d:%s)\n", errno, strerror(errno));
                 }
             } break;
+
             case TERMINATE_MESSAGE: {
                 itti_exit_task();
             } break;
+
             case MESSAGE_TEST: {
             } break;
+
             default: {
                 UDP_DEBUG("Unkwnon message ID %d:%s\n",
                           ITTI_MSG_ID(received_message_p), ITTI_MSG_NAME(received_message_p));
