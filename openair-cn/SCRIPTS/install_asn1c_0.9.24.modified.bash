29b734910e1c35f4391992fae97537a62425aa5a
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2014-04-16 15:10:52 +0000

    ok now
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5252 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair-cn/SCRIPTS/install_asn1c_0.9.24.modified.bash
+++ openair-cn/SCRIPTS/install_asn1c_0.9.24.modified.bash
@@ -1,29 +1,13 @@
 #!/bin/bash
 # Tested on ubuntu 12.04 with updates 07 november 2013
 
-###########################################################
-THIS_SCRIPT_PATH=$(dirname $(readlink -f $0))
-source $THIS_SCRIPT_PATH/utils.bash
-###########################################################
 
-if [ -f $OPENAIRCN_DIR/S1AP/MESSAGES/ASN1/asn1c-0.9.24.modified.tar.gz ]
-then
-    echo_success "Found file $OPENAIRCN_DIR/S1AP/MESSAGES/ASN1/asn1c-0.9.24.modified.tar.gz, installing..."
-    cp $OPENAIRCN_DIR/S1AP/MESSAGES/ASN1/asn1c-0.9.24.modified.tar.gz /usr/local/src
-    cd /usr/local/src
-    extract /usr/local/src/asn1c-0.9.24.modified.tar.gz
-    if [ $? -eq 0 ]; then
-        cd asn1c-9.24
-        autoreconf
-        rm configure.in
-        ./configure
-        make
-        make install
-        exit 0
-    else
-        echo_error "Failed in extracting /usr/local/src/asn1c-0.9.24.modified.tar.gz, exiting"
-    fi
-else
-    echo_error "File $OPENAIRCN_DIR/S1AP/MESSAGES/ASN1/asn1c-0.9.24.modified.tar.gz not found, please ask Eurecom developers how to get it, exiting"
-fi
-exit 1
+rm -Rf /usr/local/src/asn1c-r1516
+svn co https://github.com/vlm/asn1c/trunk  /usr/local/src/asn1c-r1516 -r 1516
+cd /usr/local/src/asn1c-r1516
+patch -p0 < $OPENAIRCN_DIR/S1AP/MESSAGES/ASN1/asn1cpatch.p0
+./configure
+make
+make install
+cd -
+
