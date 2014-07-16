78ca4cf0015021a76e06d83a174725e356873935
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2014-07-16 13:40:29 +0000

    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5516 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair-cn/SECU/key_nas_encryption.c
+++ openair-cn/SECU/key_nas_encryption.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "secu_defs.h"
 
 /*!
@@ -13,33 +14,35 @@
  * @param[in] kasme Key for MME as provided by AUC
  * @param[out] knas Truncated ouput key as derived by KDF
  */
-int derive_key_nas(algorithm_distinguisher_t nas_alg_type, uint8_t nas_enc_alg_id,
-                   uint8_t kasme[32], uint8_t knas[16])
+
+/*int derive_key_nas(algorithm_type_dist_t nas_alg_type, uint8_t nas_enc_alg_id,
+		           const uint8_t kasme[32], uint8_t** knas)
 {
     uint8_t s[7];
     uint8_t knas_temp[32];
 
-    /* FC */
+    // FC
     s[0] = 0x15;
 
-    /* P0 = algorithm type distinguisher */
+    // P0 = algorithm type distinguisher
     s[1] = nas_alg_type & 0xFF;
 
-    /* L0 = length(P0) = 1 */
+    // L0 = length(P0) = 1
     s[2] = 0x00;
     s[3] = 0x01;
 
-    /* P1 */
+    // P1
     s[4] = nas_enc_alg_id;
 
-    /* L1 = length(P1) = 1 */
+    // L1 = length(P1) = 1
     s[5] = 0x00;
     s[6] = 0x01;
 
-    kdf(s, 7, kasme, 32, knas_temp, 32);
+    kdf(s, 7, (uint8_t*)kasme, 32, (uint8_t**)&knas_temp, 32);
 
-    /* Truncate the generate key to 128 bits */
+    // Truncate the generate key to 128 bits
     memcpy(knas, knas_temp, 16);
 
     return 0;
 }
+*/
