9827c40107146ec7fe5dd7641b159f5d6b1ad8e9
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2014-08-05 07:44:54 +0000

    ITTI TRACE
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5651 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair2/COMMON/rlc_messages_def.h
+++ openair2/COMMON/rlc_messages_def.h
@@ -44,6 +44,8 @@ MESSAGE_DEF(RLC_AM_DATA_PDU_IND,                MESSAGE_PRIORITY_MED_PLUS,  Itti
 MESSAGE_DEF(RLC_AM_DATA_PDU_REQ,                MESSAGE_PRIORITY_MED_PLUS,  IttiMsgText,         rlc_am_data_pdu_req)
 MESSAGE_DEF(RLC_AM_STATUS_PDU_IND,              MESSAGE_PRIORITY_MED_PLUS,  IttiMsgText,         rlc_am_status_pdu_ind)
 MESSAGE_DEF(RLC_AM_STATUS_PDU_REQ,              MESSAGE_PRIORITY_MED_PLUS,  IttiMsgText,         rlc_am_status_pdu_req)
+MESSAGE_DEF(RLC_AM_SDU_REQ,                     MESSAGE_PRIORITY_MED_PLUS,  IttiMsgText,         rlc_am_sdu_req)
+MESSAGE_DEF(RLC_AM_SDU_IND,                     MESSAGE_PRIORITY_MED_PLUS,  IttiMsgText,         rlc_am_sdu_ind)
 
 MESSAGE_DEF(RLC_UM_DATA_PDU_IND,                MESSAGE_PRIORITY_MED_PLUS,  IttiMsgText,         rlc_um_data_pdu_ind)
 MESSAGE_DEF(RLC_UM_DATA_PDU_REQ,                MESSAGE_PRIORITY_MED_PLUS,  IttiMsgText,         rlc_um_data_pdu_req)
