f9a4838a9fa5e7ffef2a5f5e12e3c92fcfaffbac
Raymond Knopp <raymond.knopp@eurecom.fr>
2014-08-07 14:20:48 +0000

    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5683 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair-cn/SCRIPTS/install_asn1c_0.9.24.modified.bash
+++ openair-cn/SCRIPTS/install_asn1c_0.9.24.modified.bash
@@ -30,7 +30,7 @@
 # Tested on ubuntu 12.04 with updates 07 november 2013
 
 
-rm -Rf /usr/local/src/asn1c-r1516
+sudo rm -Rf /usr/local/src/asn1c-r1516
 sudo svn co https://github.com/vlm/asn1c/trunk  /usr/local/src/asn1c-r1516 -r 1516
 cd /usr/local/src/asn1c-r1516
 sudo patch -p0 < $OPENAIRCN_DIR/S1AP/MESSAGES/ASN1/asn1cpatch.p0
