b1a08dfdd11cce5ff261864e8a283c82f15a87e6
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2014-08-07 20:03:54 +0000

    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5692 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- targets/PROJECTS/GENERIC-LTE-EPC/start_epc.bash
+++ targets/PROJECTS/GENERIC-LTE-EPC/start_epc.bash
@@ -360,6 +360,7 @@ if [ "x$GNU_DEBUGGER" == "xyes" ]; then
     touch .gdbinit
     echo "file $OPENAIRCN_DIR/$OBJ_DIR/OAI_EPC/oai_epc" > .gdbinit
     echo "set args -K $THIS_SCRIPT_PATH/OUTPUT/$HOSTNAME/$ITTI_LOG_FILE -c $THIS_SCRIPT_PATH/$CONFIG_FILE_EPC" >> .gdbinit
+#    echo "b encode_eps_mobile_identity" >> .gdbinit
     echo "run" >> .gdbinit
     gdb 2>&1 | tee $THIS_SCRIPT_PATH/OUTPUT/$HOSTNAME/$STDOUT_LOG_FILE
 else 
