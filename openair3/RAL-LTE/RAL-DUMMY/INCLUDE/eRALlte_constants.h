7d8894289c9f2153c6df95f5d0e77c4ad01332d3
hrizi <hrizi@eurecom.fr>
2014-08-18 09:57:31 +0000

    openair3 OAI License
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5728 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair3/RAL-LTE/RAL-DUMMY/INCLUDE/eRALlte_constants.h
+++ openair3/RAL-LTE/RAL-DUMMY/INCLUDE/eRALlte_constants.h
@@ -1,23 +1,31 @@
-/*****************************************************************************
- *			Eurecom OpenAirInterface 3
- * 			Copyright(c) 2012 Eurecom
- *
- * Source	eRALlte_constants.h
- *
- * Version	0.1
- *
- * Date		06/26/2012
- *
- * Product	MIH RAL LTE
- *
- * Subsystem	RAL-LTE constants
- *
- * Authors	Michelle Wetterwald, Lionel Gauthier, Frederic Maurel
- *
- * Description	Defines constants used by the RAL-LTE process at the network
- *		side.
- *
- *****************************************************************************/
+/*******************************************************************************
+    OpenAirInterface 
+    Copyright(c) 1999 - 2014 Eurecom
+
+    OpenAirInterface is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+
+    OpenAirInterface is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenAirInterface.The full GNU General Public License is 
+   included in this distribution in the file called "COPYING". If not, 
+   see <http://www.gnu.org/licenses/>.
+
+  Contact Information
+  OpenAirInterface Admin: openair_admin@eurecom.fr
+  OpenAirInterface Tech : openair_tech@eurecom.fr
+  OpenAirInterface Dev  : openair4g-devel@eurecom.fr
+  
+  Address      : Eurecom, Campus SophiaTech, 450 Route des Chappes, CS 50193 - 06904 Biot Sophia Antipolis cedex, FRANCE
+
+ *******************************************************************************/
 
 #ifndef __RAL_LTE_CONS_H__
 #define __RAL_LTE_CONS_H__
--- openair3/RAL-LTE/RAL-DUMMY/INCLUDE/nas_ue_ioctl.h
+++ openair3/RAL-LTE/RAL-DUMMY/INCLUDE/nas_ue_ioctl.h
@@ -1,12 +1,31 @@
-/***************************************************************************
-                          nas_ue_ioctl.h  -  description
-                             -------------------
-    copyright            : (C) 2005 by Eurecom
-    email                : michelle.wetterwald@eurecom.fr
-                           yan.moret@eurecom.fr
- ***************************************************************************
+/*******************************************************************************
+    OpenAirInterface 
+    Copyright(c) 1999 - 2014 Eurecom
 
- ***************************************************************************/
+    OpenAirInterface is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+
+    OpenAirInterface is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenAirInterface.The full GNU General Public License is 
+   included in this distribution in the file called "COPYING". If not, 
+   see <http://www.gnu.org/licenses/>.
+
+  Contact Information
+  OpenAirInterface Admin: openair_admin@eurecom.fr
+  OpenAirInterface Tech : openair_tech@eurecom.fr
+  OpenAirInterface Dev  : openair4g-devel@eurecom.fr
+  
+  Address      : Eurecom, Campus SophiaTech, 450 Route des Chappes, CS 50193 - 06904 Biot Sophia Antipolis cedex, FRANCE
+
+ *******************************************************************************/
 #ifndef _IAL_IOCTL_H
 #define _IAL_IOCTL_H
 
