af31c053c4914e45bccb7e14a3d18f0abc41fe6a
Navid Nikaein <navid.nikaein@eurecom.fr>
2014-09-10 08:22:02 +0000

    add rati modules
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5779 818b1a75-f10b-46b9-bf7c-635c3b92a50f

---
+++ targets/RT/USER/init_rtai.sh
@@ -0,0 +1,5 @@
+sudo insmod /usr/realtime/modules/rtai_hal.ko 
+sudo insmod /usr/realtime/modules/rtai_sched.ko 
+sudo insmod /usr/realtime/modules/rtai_sem.ko 
+sudo insmod /usr/realtime/modules/rtai_fifos.ko 
+sudo insmod /usr/realtime/modules/rtai_mbx.ko 
