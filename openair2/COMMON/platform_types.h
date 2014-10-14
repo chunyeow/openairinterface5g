1a4b1d928ade948eb11e08533629a521e5bb5908
Raymond Knopp <raymond.knopp@eurecom.fr>
2014-10-14 12:57:01 +0000

    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@5884 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair2/COMMON/platform_types.h
+++ openair2/COMMON/platform_types.h
@@ -65,7 +65,8 @@ typedef uint32_t              frame_t;
 typedef int32_t               sframe_t;
 typedef uint32_t              sub_frame_t;
 typedef uint8_t               module_id_t;
-typedef int16_t                smodule_id_t;
+typedef uint16_t              ue_id_t;
+typedef int16_t               smodule_id_t;
 typedef uint16_t              rb_id_t;
 typedef uint16_t              srb_id_t;
 
