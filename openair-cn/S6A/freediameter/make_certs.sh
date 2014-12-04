ca95fb7924905caed42114f86513035b6c803703
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2014-12-04 12:07:26 +0000

    arg is realm
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@6154 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair-cn/S6A/freediameter/make_certs.sh
+++ openair-cn/S6A/freediameter/make_certs.sh
@@ -28,6 +28,13 @@
 #
 ################################################################################
 
+# ARG is REALM
+# BY DEFAULT REALM IS "eur"
+
+DEFAULTREALMVALUE="eur"
+echo "ARG is $1"
+REALM=${1:-$DEFAULTREALMVALUE}
+
 rm -rf demoCA
 mkdir demoCA
 echo 01 > demoCA/serial
@@ -36,13 +43,13 @@ touch demoCA/index.txt
 user=$(whoami)
 HOSTNAME=$(hostname -f)
 
-echo "Creating certificate for user '$HOSTNAME'"
+echo "Creating certificate for user '$HOSTNAME'.'$REALM'"
 
 # CA self certificate
-openssl req  -new -batch -x509 -days 3650 -nodes -newkey rsa:1024 -out cacert.pem -keyout cakey.pem -subj /CN=eur/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
+openssl req  -new -batch -x509 -days 3650 -nodes -newkey rsa:1024 -out cacert.pem -keyout cakey.pem -subj /CN=$REALM/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
 
 openssl genrsa -out user.key.pem 1024
-openssl req -new -batch -out user.csr.pem -key user.key.pem -subj /CN=$HOSTNAME.eur/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
+openssl req -new -batch -out user.csr.pem -key user.key.pem -subj /CN=$HOSTNAME.$REALM/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
 openssl ca -cert cacert.pem -keyfile cakey.pem -in user.csr.pem -out user.cert.pem -outdir . -batch
 
 if [ ! -d /usr/local/etc/freeDiameter ]
