75998d02e6b5c24ee276bcb6a38c77c080b48494
kortke <kortke@eurecom.fr>
2015-01-27 15:52:36 +0000

    fixes for 2 channel RX USRP path
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@6434 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- targets/ARCH/USRP/USERSPACE/LIB/usrp_lib.cpp
+++ targets/ARCH/USRP/USERSPACE/LIB/usrp_lib.cpp
@@ -292,7 +292,7 @@ int openair0_device_init(openair0_device* device, openair0_config_t *openair0_cf
   for(i=0;i<s->usrp->get_rx_num_channels();i++) {
     if (i<openair0_cfg[0].rx_num_channels) {
       s->usrp->set_rx_rate(openair0_cfg[0].sample_rate,i);
-      s->usrp->set_rx_bandwidth(openair0_cfg[0].rx_bw);
+      s->usrp->set_rx_bandwidth(openair0_cfg[0].rx_bw,i);
       printf("Setting rx freq/gain on channel %lu/%lu\n",i,s->usrp->get_rx_num_channels());
       s->usrp->set_rx_freq(openair0_cfg[0].rx_freq[i],i);
       s->usrp->set_rx_gain(openair0_cfg[0].rx_gain[i],i);
