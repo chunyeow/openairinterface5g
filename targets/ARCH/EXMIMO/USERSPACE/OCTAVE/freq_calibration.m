e0b90b9c2b04f925ad30bf617172d6e08983c2f5
Florian Kaltenberger <florian.kaltenberger@eurecom.fr>
2015-03-23 11:08:34 +0000

    fix in freq_calibration.m
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@6866 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- targets/ARCH/EXMIMO/USERSPACE/OCTAVE/freq_calibration.m
+++ targets/ARCH/EXMIMO/USERSPACE/OCTAVE/freq_calibration.m
@@ -11,7 +11,8 @@ cables_loss_dB = 6;    % we need to account for the power loss between the signa
 dual_tx = 0;
 tdd = 1;
 
-fc = 1907600e3;   % this has to be the same as in the config file
+fc = 1907600e3;
+%fc = 2.68e9;
 fs = 7680e3;
 %fs = 6500e3;
 fref = fc+fs/4;
@@ -23,7 +24,7 @@ limeparms;
 rf_mode = (RXEN+0+TXLPFNORM+TXLPFEN+TXLPF25+RXLPFNORM+RXLPFEN+RXLPF25+LNA1ON+LNAMax+RFBBNORM+DMAMODE_RX+0)*[1 0 0 0];
 freq_rx = fc*[1 0 0 0];
 freq_tx = freq_rx+1920000;
-rx_gain = 0*[1 1 1 1];
+rx_gain = 30*[1 1 1 1];
 tx_gain = 25*[1 1 1 1];
 %rf_local=rf_local*[1 1 1 1];
 rf_local = [8254813 8255016 8254813 8254813]; %exmimo2_2
@@ -36,6 +37,7 @@ rffe_rxg_low = 63*[1 1 1 1];
 rffe_rxg_final = 31*[1 1 1 1];
 rffe_band = B19G_TDD*[1 1 1 1];
 autocal = [1 1 1 1];
+resampling = [2 2 2 2];
 
 %gpib_send(gpib_card,gpib_device,'*RST;*CLS');   % reset and configure the signal generator
 %gpib_send(gpib_card,gpib_device,['POW ' int2str(power_dBm+cables_loss_dB) 'dBm']);
@@ -56,11 +58,12 @@ do
   fc
   freq_rx = fc*[1 0 0 0];
   freq_tx = freq_rx+1920000;
-  oarf_config_exmimo(card, freq_rx,freq_tx,tdd_config,syncmode,rx_gain,tx_gain,0,rf_mode,rf_rxdc,rf_local,rf_vcocal,rffe_rxg_low,rffe_rxg_final,rffe_band,autocal);
+  oarf_config_exmimo(card, freq_rx,freq_tx,tdd_config,syncmode,rx_gain,tx_gain,0,rf_mode,rf_rxdc,rf_local,rf_vcocal,rffe_rxg_low,rffe_rxg_final,rffe_band,autocal,resampling);
 
   i=i+1;
   sleep(1);
   s=oarf_get_frame(card);   %oarf_get_frame
+  s(76801:end,:)=[];
 
   nb_rx = 1; %size(s,2);
 
