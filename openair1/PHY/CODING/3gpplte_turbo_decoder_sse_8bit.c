09044dda34f5a8bc48c38a10410ee508da4ff0ef
Lionel Gauthier <lionel.gauthier@eurecom.fr>
2015-04-09 09:21:50 +0000

    patches13/0023-not-sure-how-to-fix-this.patch
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@7069 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair1/PHY/CODING/3gpplte_turbo_decoder_sse_8bit.c
+++ openair1/PHY/CODING/3gpplte_turbo_decoder_sse_8bit.c
@@ -367,7 +367,10 @@ void compute_beta8(llr_t* alpha,llr_t* beta,llr_t *m_11,llr_t* m_10,unsigned sho
        loopval=(frame_length>>4)-L,rerun_flag++) {
 
     if (offset8_flag==0) {
-      DevMessage( "beta0-beta7 are used uninitialized. FIXME!" );
+      // FIXME! beta0-beta7 are used uninitialized. FIXME!
+      // workaround: init with 0
+      beta0 = beta1 = beta2 = beta3 = beta4 = beta5 = beta6 = beta7 = 0;
+
       beta_ptr[0] = _mm_insert_epi8(beta_ptr[0],beta0,15);
       beta_ptr[1] = _mm_insert_epi8(beta_ptr[1],beta1,15);
       beta_ptr[2] = _mm_insert_epi8(beta_ptr[2],beta2,15);
