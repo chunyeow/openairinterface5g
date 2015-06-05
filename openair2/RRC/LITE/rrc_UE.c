9aa4543ab93c4549cccc493373b3a0dfbb707bae
Florian Kaltenberger <florian.kaltenberger@eurecom.fr>
2015-06-05 13:46:25 +0000

    fix for real-time UE (protected some code with OAI_EMU)
    git-svn-id: http://svn.eurecom.fr/openair4G/trunk@7534 818b1a75-f10b-46b9-bf7c-635c3b92a50f

--- openair2/RRC/LITE/rrc_UE.c
+++ openair2/RRC/LITE/rrc_UE.c
@@ -1281,7 +1281,7 @@ rrc_ue_process_radioResourceConfigDedicated(
 
   UE_rrc_inst[ctxt_pP->module_id].Info[eNB_index].State = RRC_CONNECTED;
   LOG_I(RRC,"[UE %d] State = RRC_CONNECTED (eNB %d)\n",ctxt_pP->module_id,eNB_index);
-#if !defined(ENABLE_USE_MME)
+#if !defined(ENABLE_USE_MME) && defined(OAI_EMU)
   rrc_eNB_emulation_notify_ue_module_id(
     ctxt_pP->module_id,
     ctxt_pP->rnti,
